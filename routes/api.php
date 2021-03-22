eefebd9dfe4aa072621b66642a8606737552d889
Eslamelarabi <45950077+Eslamelarabi@users.noreply.github.com>
2021-03-22 14:58:33 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/movies', 'MoviesController@index');
-Route::get('/movies/{id}', 'MoviesController@show');
 Route::get('/movies/rated', 'MoviesController@getRated');
+Route::get('/movies/{id}', 'MoviesController@show');
+
